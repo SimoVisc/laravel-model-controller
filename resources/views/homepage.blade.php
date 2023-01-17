30049b5280b60010e7f230947c4d5f5f54af9bc3
SimoVisc <simonaviscardivs@gmail.com>
2023-01-17 12:27:14 +0100

    Aggiunge il batabase Movies

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -19,6 +19,12 @@
 
     <main>
         <h1> Homepage </h1>
+        <ul>
+            @foreach ($movies as $movie)
+                <li> {{ $movie->title }}</li>
+            @endforeach
+
+        </ul>
     </main>
 
 </body>
